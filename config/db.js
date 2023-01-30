94911f2518de21960ea05770d14f55b50250a519
RishiPatel0290 <98559879+RishiPatel2020@users.noreply.github.com>
2023-01-29 23:55:13 -0500

    testing if it hits api3

--- config/db.js
+++ config/db.js
@@ -1,17 +1,35 @@
-require("dotenv").config();
+// require("dotenv").config();
 const mysql = require("mysql2");
 
+// const pool = mysql.createPool({
+//     // host: process.env.DB_HOST,
+//     // user: process.env.DB_USER /* MySQL User */,
+//     // password: process.env.DB_PASSWORD /* MySQL Password */,
+//     // database: process.env.DB_NAME /* MySQL Database */,
+//     host: "containers-us-west-186.railway.app",
+//     user: "root" /* MySQL User */,
+//     password: "Jnna6d0hSQ7oum5GK6WR" /* MySQL Password */,
+//     database: "railway" /* MySQL Database */,
+//     port:"6748"
+//   });;
+
+
 const pool = mysql.createPool({
-    // host: process.env.DB_HOST,
-    // user: process.env.DB_USER /* MySQL User */,
-    // password: process.env.DB_PASSWORD /* MySQL Password */,
-    // database: process.env.DB_NAME /* MySQL Database */,
-    host: "containers-us-west-186.railway.app",
-    user: "root" /* MySQL User */,
-    password: "Jnna6d0hSQ7oum5GK6WR" /* MySQL Password */,
-    database: "railway" /* MySQL Database */,
-    port:"6748"
-  });;
+  host: "containers-us-west-186.railway.app",
+  user: "root",
+  password: "Jnna6d0hSQ7oum5GK6WR",
+  database: "railway",
+  port:"6748"
+});
 
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to database: ', err.stack);
+    return;
+  }
 
-module.exports = pool.promise(); 
+  console.log('Connected to database as id ', connection.threadId);
+  connection.release();
+});
+
+// module.exports = pool.promise(); 
