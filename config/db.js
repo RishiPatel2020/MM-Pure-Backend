6dee0b532ef388aecac500c1809c122850272a5b
RishiPatel0290 <98559879+RishiPatel2020@users.noreply.github.com>
2023-01-29 23:33:04 -0500

    testing if it hits api3

--- config/db.js
+++ config/db.js
@@ -2,10 +2,14 @@ require("dotenv").config();
 const mysql = require("mysql");
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER /* MySQL User */,
-    password: process.env.DB_PASSWORD /* MySQL Password */,
-    database: process.env.DB_NAME /* MySQL Database */,
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USER /* MySQL User */,
+    // password: process.env.DB_PASSWORD /* MySQL Password */,
+    // database: process.env.DB_NAME /* MySQL Database */,
+    host: "containers-us-west-186.railway.app",
+    user: "root" /* MySQL User */,
+    password: "Jnna6d0hSQ7oum5GK6WR" /* MySQL Password */,
+    database: "railway" /* MySQL Database */,
   });;
 
 
