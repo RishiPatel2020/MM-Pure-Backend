146d0a28c28a5c74ebae47f6b50b78078f3481ac
RishiPatel0290 <98559879+RishiPatel2020@users.noreply.github.com>
2023-02-01 17:40:37 -0500

    stripe changed to basic

--- routes/checkout.js
+++ routes/checkout.js
@@ -5,35 +5,35 @@ const Order = require("../models/Order");
 const User = require("../models/User");
 
 // Add line items
-// router.post("/payment",(req,res)=>{
-//     stripe.charges.create({
-//         source:req.body.tokenId,
-//         amount:req.body.amount,
-//         currency:"usd",
-//         receipt_email:req.body.email
+router.post("/payment",(req,res)=>{
+    stripe.charges.create({
+        source:req.body.tokenId,
+        amount:req.body.amount,
+        currency:"usd",
+        receipt_email:req.body.email
 
-//     },(stripeErr,stripeRes)=>{
-//         if(stripeErr){
-//             console.log("ERROR... "+stripeErr);
-//             res.status(206).send(""+stripeErr);
-//         }else{
-//             res.status(200).json(stripeRes);
-//         }
-//     });
-// });
-router.post("/payment", async (req, res) => {
-  try {
-    const [result] = await stripe.charges.create({
-      source: req.body.tokenId,
-      amount: req.body.amount,
-      currency: "usd",
-      receipt_email: req.body.email,
+    },(stripeErr,stripeRes)=>{
+        if(stripeErr){
+            console.log("ERROR... "+stripeErr);
+            res.status(206).send(""+stripeErr);
+        }else{
+            res.status(200).json(stripeRes);
+        }
     });
-    res.status(200).json(result);
-  } catch (err) {
-    res.status(206).send(err);
-  }
 });
+// router.post("/payment", async (req, res) => {
+//   try {
+//     const [result] = await stripe.charges.create({
+//       source: req.body.tokenId,
+//       amount: req.body.amount,
+//       currency: "usd",
+//       receipt_email: req.body.email,
+//     });
+//     res.status(200).json(result);
+//   } catch (err) {
+//     res.status(206).send(err);
+//   }
+// });
 // this is where we register the order in our DB
 router.post("/addOrder", (req, res) => {
   Order.add(req.body, res);
