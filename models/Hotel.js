5996d1582d71bc6c366c75229dcc2472a5af07b6
RishiPatel0290 <98559879+RishiPatel2020@users.noreply.github.com>
2023-02-05 17:55:16 -0500

    Hotel portal table fixed

--- models/Hotel.js
+++ models/Hotel.js
@@ -25,13 +25,9 @@ class Hotel {
   // PREPARES TABLE 2
   async getOrdersTable(date, resp) {
     try {
-      // const q =
-      //   "Select ot.id,i.Name,h.quantity,ot.Shipping_Date FROM Order_table ot JOIN Hotel H On ot.id = h.Order_ID JOIN Item i On H.Item_ID = i.Item_ID Where ot.Shipping_date = ? ORDER BY ot.Order_date";
-
-      // Uncomment above to get order history by closest sunday!
       const q =
-        "Select ot.id,i.Name,h.Quantity,ot.Shipping_Date FROM Order_table ot JOIN Hotel h On ot.id = h.Order_ID JOIN Item i On h.Item_ID = i.Item_ID ORDER BY ot.Order_date";
-      const [result] = await mysql.query(q);
+        "Select ot.id,i.Name,h.Quantity,ot.Shipping_Date FROM Order_table ot JOIN Hotel h On ot.id = h.Order_ID JOIN Item i On h.Item_ID = i.Item_ID Where ot.Shipping_date = ? ORDER BY ot.Order_date";
+      const [result] = await mysql.query(q,[date]);
       const temp = {};
       result.forEach((element) => {
         if (temp[element.id]) {
@@ -54,14 +50,9 @@ class Hotel {
   // PREPARES TABLE 1
   async getMealQuantityTable(date, resp) {
     try {
-      // const q =
-      //   "Select  Hotel.item_id, sum(Hotel.Quantity) as Total_Quantity From Order_table ot JOIN Hotel on ot.id = Hotel.order_id Where ot.Shipping_date = ? Group By item_ID";
-
-      // Uncomment above to get order history by closest sunday!
-
       const q =
-        "Select  Hotel.Item_ID as item_id, sum(Hotel.Quantity) as Total_Quantity From Order_table ot JOIN Hotel on ot.id = Hotel.Order_Id Group By Item_ID";
-      const [result] = await mysql.query(q);
+        "Select  Hotel.Item_ID as item_id, sum(Hotel.Quantity) as Total_Quantity From Order_table ot JOIN Hotel on ot.id = Hotel.Order_Id Where ot.Shipping_date = ? Group By Item_ID";
+      const [result] = await mysql.query(q,[date]);
       resp.status(200).json(result);
     } catch (err) {
       resp.status(206).json(err);
