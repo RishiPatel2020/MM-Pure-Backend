f13b7e356e8c1d1c5c09632f5f7a30d7736e13f5
RishiPatel0290 <98559879+RishiPatel2020@users.noreply.github.com>
2023-02-12 23:59:43 -0500

    trying diff time for SMS

--- models/BackgroundTasks.js
+++ models/BackgroundTasks.js
@@ -24,7 +24,7 @@ async function reminder() {
 
 // replace 24 23 with 0 18 for 06:00PM EST
 module.exports = function () {
-  cron.schedule("0 18 * * 6,0", function () {
+  cron.schedule("0 23 * * 0,6,1", function () {
     console.log("SEND SMSs NOW!!!");
     reminder();
   });
